b76cce1596f0612dd882c041413789165dc1e01c
Simen Ringdahl <simen.ringdahl@gmail.com>
2025-03-21 08:49:35 +0100

    Sette opp nettsiden

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
-from PIL import Image
-import pytesseract
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
-#import pyrebase
+
 
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/drive']
@@ -16,21 +14,20 @@ sheet = client.open("Tankbil-CC").sheet1
 st.set_page_config(
     page_title="Vektregistrering Carbon Centric",
     page_icon="📱",
-    layout="wide",  # Bruk "wide" for bedre plass på mobil
+   # layout="wide",  # Bruk "wide" for bedre plass på mobil
     initial_sidebar_state="collapsed"
 )
 
-st.markdown("<h2>Vektregistrering Carbon Centric</h2>", unsafe_allow_html=True)
+st.markdown("<h2>Vektregistrering Carbon Centric AS</h2>", unsafe_allow_html=True)
 st.markdown("</div>", unsafe_allow_html=True)
 
 
-# Lager litt info bare for å vise frem:
 
 # Forslag til registreringsnummer basert på dato (mock)
 heutige_dato = datetime.now().strftime('%Y-%m-%d')
 forventede_biler = {
-    "2025-03-19": ["AB12345", "CD67890"],
-    "2025-03-20": ["EF12345"]
+    "2025-03-20": ["0001", "0002"],
+    "2025-03-21": ["EF12345"]
 }
 
 forslag_regnr = forventede_biler.get(heutige_dato, [])
@@ -38,17 +35,9 @@ regnr = st.selectbox("Batchnummer", forslag_regnr + ["Annet..."])
 if regnr == "Annet...":
     regnr = st.text_input("Skriv inn batchnummer")
 
-# Last opp bilde og kjør OCR
-# uploaded_file = st.file_uploader("Last opp bilde av vekt-display")
+
 vekt_detected = ""
 
-# if uploaded_file:
-#     image = Image.open(uploaded_file)
-#     st.image(image, caption="Opplastet bilde", use_column_width=True)
-#     vekt_detected = pytesseract.image_to_string(image, config='--psm 7 digits')
-#     st.write(f"Gjenkjent vekt: {vekt_detected.strip()}")
-
-# Tillat overstyring
 vekt_input = st.text_input("Vekt (kg)", value=vekt_detected.strip())
 
 # Send til Google Sheet
